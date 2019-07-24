9b78bd5a3e38f7564117bfbb2687b4bf3c6baf4e
Richard Feder <richardfederstaehle@college.harvard.edu>
2019-07-24 13:10:55 -0700

    added code for computing mode coupling matrix given mask

--- cross_spectrum_analysis.py
+++ cross_spectrum_analysis.py
@@ -2,37 +2,6 @@ import numpy as np
 from ciber_mocks import *
 
 
-flight=40030 
-inst=1  #inst=1-I band magnitude,2-H band magnitude
-ifield=4  #use the PSF in which CIBER field? 4-elat10,5-elat30,6-BootesB,7-BootesA,8-SWIRE
-field='XMM' # which HSC field?
-m_min=10 
-m_max=27 # magnitude limits
-usePSF=False # if false, don't convolve with PSF, just put all the light in the central pixel
-nbin = 1
-
-def get_bin_idxs(arr, bins):
-    i=0
-    maxval = np.max(arr)
-    idxs = [0]
-    for ind, val in enumerate(arr):
-        if val-bins[i+1]>=0:
-            idxs.append(ind)
-            if i==len(bins)-1:
-                return idxs
-            else:
-                i+=1
-        elif val == maxval:
-            idxs.append(ind)
-            return idxs
-
-
-def compute_cross_spectrum(map_a, map_b):
-    ffta = np.fft.fft2(map_a)
-    fftb = np.fft.fft2(map_b)
-    xspectrum = np.abs(ffta*np.conj(fftb)+fftb*np.conj(ffta))
-    return np.fft.fftshift(xspectrum)
-
 def azimuthalAverage(image, lmin=90, center=None, logbins=True, nbins=60):
     """
     Calculate the azimuthally averaged radial profile.
@@ -69,11 +38,11 @@ def azimuthalAverage(image, lmin=90, center=None, logbins=True, nbins=60):
     
     # convert multipole bins into pixel values
     radbins /= np.min(radbins)
-        
-    
     rbin_idxs = get_bin_idxs(r_sorted, radbins)
+
     rad_avg = []
     rad_std = []
+    
     for i in xrange(len(rbin_idxs)-1):
         nmodes= len(i_sorted[rbin_idxs[i]:rbin_idxs[i+1]])
         rad_avg.append(np.mean(i_sorted[rbin_idxs[i]:rbin_idxs[i+1]]))
@@ -83,6 +52,89 @@ def azimuthalAverage(image, lmin=90, center=None, logbins=True, nbins=60):
     av_rbins = (radbins[:-1]+radbins[1:])/2
 
     return av_rbins, np.array(rad_avg), np.array(rad_std)
+
+
+def compute_cross_spectrum(map_a, map_b, dim=2.0, map_a_binary=False, map_b_binary=False):
+    dim_use = dim*np.pi/180.
+    steradperpixel = (dim_use/map_a.shape[0])**2
+    
+    map_a2 = map_a*steradperpixel
+    map_b2 = map_b*steradperpixel
+    
+    ffta = np.fft.fft2(map_a2)
+    fftb = np.fft.fft2(map_b2)
+    xspectrum = np.abs(ffta*np.conj(fftb)+fftb*np.conj(ffta))
+    
+    return np.fft.fftshift(xspectrum)
+
+
+def compute_mode_coupling(mask, ell_min=90., nphases=50, logbins=True, nbins=60):
+    ell_max = ell_min*np.sqrt(2*(mask.shape[0]/2)**2)
+    
+    if logbins:
+        radbins = 10**(np.linspace(np.log10(ell_min), np.log10(ell_max), nbins))
+    else:
+        radbins = np.linspace(lmin, lmax, nbins+1)
+        
+    print 'ell bins:'
+    print radbins
+    
+    Mkk = np.zeros((radbins.shape[0], radbins.shape[0]))
+    sigma_Mkk = np.zeros((radbins.shape[0], radbins.shape[0]))
+    ps_amplitude = 100.0
+        
+    for i, radbin in enumerate(radbins):
+        ps = np.zeros_like(radbins)
+        ps[i] = ps_amplitude
+        grfs, _ = grf_mkk(nphases, size=mask.shape[0], ps=ps, ell_sampled=radbins)
+
+        masked_grfs = grfs*mask
+        masked_ps = compute_cross_spectrum(masked_grfs, masked_grfs)
+        
+        norm_radavs = []
+        for j, spec in enumerate(masked_ps):
+            _, norm_radav, _ = azimuthalAverage(spec, nbins=nbins)
+            norm_radavs.append(norm_radav/ps_amplitude)
+        norm_radavs = np.array(norm_radavs)   
+                
+        Mkk[i,:] = np.mean(norm_radavs, axis=0)
+        sigma_Mkk[i,:] = np.std(norm_radavs, axis=0)
+        
+        plt.figure()
+        plt.title('$\\ell=$'+str(radbin))
+        plt.imshow(grfs[0]*mask)
+        plt.colorbar()
+        plt.show()
+        
+    return Mkk, sigma_Mkk
+
+
+def compute_mode_coupling(mask, ell_min=90., nphases=50, logbins=True, nbins=60):
+    ell_max = ell_min*np.sqrt(2*(mask.shape[0]/2)**2)
+    
+    if logbins:
+        radbins = 10**(np.linspace(np.log10(ell_min), np.log10(ell_max), nbins+1))
+    else:
+        radbins = np.linspace(lmin, lmax, nbins+1)
+        
+    print(radbins)
+        
+    for i, radbin in enumerate(radbins):
+        ps = np.zeros_like(radbins)
+        ps[i] = 100.0
+        
+        grfs, _ = gaussian_random_fieldb(nphases, size=mask.shape[0], ps=ps, ksampled=radbins)
+
+        masked_grfs = grfs*mask
+        power_spec = compute_cross_spectrum(masked_grfs, masked_grfs)
+        print(power_spec.shape)
+        norm_radavs = []
+        for spec in power_spec:
+            _, norm_radav, _ = azimuthalAverage(spec)
+            norm_radavs.append(norm_radav)
+        norm_radavs = np.array(norm_radavs)    
+        mean_radavs = np.mean(norm_radavs, axis=0)
+        
 
 
 def cross_correlate_galcat_ciber(cibermap, galaxy_catalog, m_min=14, m_max=30, band='J', \
@@ -95,57 +147,52 @@ def cross_correlate_galcat_ciber(cibermap, galaxy_catalog, m_min=14, m_max=30, b
 
 
 
-
-def xcorr_varying_ihl(ihl_min_frac=0.0, ihl_max_frac=0.5, nbins=10, nsrc=100, m_min=14, m_max=20, gal_comp=21):
-    radprofs = []
-    radstds = []
-    cmock = ciber_mock()
-    ihl_range = np.linspace(ihl_min_frac, ihl_max_frac, nbins)
-    for i, ihlfrac in enumerate(ihl_range):
-        if i==0:
-            full, srcs, noise, cat = cmock.make_ciber_map(ifield, m_min, m_max, band=inst, nsrc=nsrc, ihl_frac=ihlfrac)
-            gal_map = make_galaxy_binary_map(cat, full, m_min, m_max=gal_comp, magidx=5) # cut off galaxy catalog at 20th mag
-        else:
-            full, srcs, noise, ihl, cat = cmock.make_ciber_map(ifield, m_min, m_max, mock_cat=cat, band=inst, nsrc=nsrc, ihl_frac=ihlfrac)
-         
-        xcorr = compute_cross_spectrum(full, gal_map)
-        rb, radprof, radstd = azimuthalAverage(xcorr)
-        radprofs.append(radprof)
-        radstds.append(radstd)
-        
-    return ihl_range, rb, radprofs, radstds
+def get_bin_idxs(arr, bins):
+    i=0
+    maxval = np.max(arr)
+    idxs = [0]
+    for ind, val in enumerate(arr):
+        if val-bins[i+1]>=0:
+            idxs.append(ind)
+            if i==len(bins)-1:
+                return idxs
+            else:
+                i+=1
+        elif val == maxval:
+            idxs.append(ind)
+            return idxs
 
 
-def xcorr_varying_galcat_completeness(ihl_frac=0.1, compmin=18, compmax=22, nbin=10, nsrc=100):
-    radprofs, radstds = [], []
-    cmock = ciber_mock()
-    comp_range = np.linspace(compmin, compmax, nbin)
-    full, srcs, noise, ihl, gal_cat = cmock.make_ciber_map(ifield, m_min, 25, band=inst, nsrc=nsrc, ihl_frac=ihl_frac)
+def grf_mkk(n_samples, size = 100, ps=None, ell_sampled=None):
 
-    for i, comp in enumerate(comp_range):
-        gal_map = make_galaxy_binary_map(gal_cat, full, m_min, m_max=comp)        
-        xcorr = compute_cross_spectrum(full, gal_map)
-        rb, radprof, radstd = azimuthalAverage(xcorr)
-        radprofs.append(radprof)
-        radstds.append(radstd)
-        
-    return comp_range, rb, radprofs, radstds
+    grfs = np.zeros((n_samples, size, size))
+    noise = np.fft.fft2(np.random.normal(size = (n_samples, size, size)))
+    amplitude = np.zeros((size, size))
+    for i, sx in enumerate(fftIndgen(size)):
+        amplitude[i,:] = Pk2_mkk(sx, np.array(fftIndgen(size)), ps=ps, ell_sampled=ell_sampled, size=size)
+    grfs = np.fft.ifft2(noise * amplitude, axes=(-2,-1))
+    
+    return grfs.real, np.array(noise*amplitude)
 
 
 def integrated_xcorr_multiple_redshifts(ihl_frac=0.1, \
-    gal_maxmag=22, \
-    zmin=0.0, \
-    zmax=5, \
-    nbin=10, \
-    nsrc=100):
+    gal_maxmag=22, 
+    zmin=0.0, 
+    zmax=5, 
+    nbin=10, 
+    nsrc=100,
+    ifield=4,
+    m_min=10, 
+    m_max=27,
+    inst=1):
     
     wints = []
     cmock = ciber_mock()
     zrange = np.linspace(zmin, zmax, nbin)
     if ihl_frac > 0:
-        full, srcs, noise, ihl, gal_cat = cmock.make_ciber_map(ifield, m_min, 25, band=inst, nsrc=nsrc, ihl_frac=ihl_frac)
+        full, srcs, noise, ihl, gal_cat = cmock.make_ciber_map(ifield, m_min, gal_maxmag, band=inst, nsrc=nsrc, ihl_frac=ihl_frac)
     else:
-        full, srcs, noise, gal_cat = cmock.make_ciber_map(ifield, m_min, 25, band=inst, nsrc=nsrc, ihl_frac=ihl_frac)
+        full, srcs, noise, gal_cat = cmock.make_ciber_map(ifield, m_min, gal_maxmag, band=inst, nsrc=nsrc, ihl_frac=ihl_frac)
         
     for i in xrange(len(zrange)-1):
         rb, radprof, radstd, xcorr = cross_correlate_galcat_ciber(full, gal_cat, m_max=gal_maxmag, zmin=zrange[i], zmax=zrange[i+1], zidx=3)
@@ -179,7 +226,45 @@ def integrate_C_l(ls, C, weights=None):
     C_integrand *= dls
     return np.sum(C_integrand)
 
+def Pk2_mkk(sx, sy, ps, ell_sampled=None, pixsize=3.39e-5, size=512.0):
+    ells = np.sqrt((sx**2+sy**2))*90.
+    idx1 = np.array([np.abs(ell_sampled-ell).argmin() for ell in ells])
+    return ps[idx1]
 
+def xcorr_varying_ihl(ihl_min_frac=0.0, ihl_max_frac=0.5, nbins=10, nsrc=100, m_min=14, m_max=20, gal_comp=21, ifield=4, inst=1):
+    radprofs = []
+    radstds = []
+    cmock = ciber_mock()
+    ihl_range = np.linspace(ihl_min_frac, ihl_max_frac, nbins)
+    for i, ihlfrac in enumerate(ihl_range):
+        if i==0:
+            full, srcs, noise, cat = cmock.make_ciber_map(ifield, m_min, m_max, band=inst, nsrc=nsrc, ihl_frac=ihlfrac)
+            gal_map = make_galaxy_binary_map(cat, full, m_min, m_max=gal_comp, magidx=5) # cut off galaxy catalog at 20th mag
+        else:
+            full, srcs, noise, ihl, cat = cmock.make_ciber_map(ifield, m_min, m_max, mock_cat=cat, band=inst, nsrc=nsrc, ihl_frac=ihlfrac)
+         
+        xcorr = compute_cross_spectrum(full, gal_map)
+        rb, radprof, radstd = azimuthalAverage(xcorr)
+        radprofs.append(radprof)
+        radstds.append(radstd)
+        
+    return ihl_range, rb, radprofs, radstds
+
+
+def xcorr_varying_galcat_completeness(ihl_frac=0.1, compmin=18, compmax=22, nbin=10, nsrc=100, ifield=4, m_min=10, inst=1):
+    radprofs, radstds = [], []
+    cmock = ciber_mock()
+    comp_range = np.linspace(compmin, compmax, nbin)
+    full, srcs, noise, ihl, gal_cat = cmock.make_ciber_map(ifield, m_min, 25, band=inst, nsrc=nsrc, ihl_frac=ihl_frac)
+
+    for i, comp in enumerate(comp_range):
+        gal_map = make_galaxy_binary_map(gal_cat, full, m_min, m_max=comp)        
+        xcorr = compute_cross_spectrum(full, gal_map)
+        rb, radprof, radstd = azimuthalAverage(xcorr)
+        radprofs.append(radprof)
+        radstds.append(radstd)
+        
+    return comp_range, rb, radprofs, radstds
 
 
 
--- plotting_fns.py
+++ plotting_fns.py
@@ -2,34 +2,34 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
-import seaborn as sns
-sns.set()
+# import seaborn as sns
+# sns.set()
 
 
 
 figure_directory = '/Users/richardfeder/Documents/caltech/ciber2/figures/'
 
-def plot_radavg_xspectrum(rbins, radprofs=[], raderrs=None, labels=[], lmin=90., save=False, snspalette=None, pdf_or_png='png'):
-	if snspalette is None:
-		sns.set_palette(sns.color_palette("Blues"))
-	else:
-		sns.set_palette(sns.color_palette(snspalette))
-	plt.figure()
-	plt.title('Cross Spectrum', fontsize=16)
-	for i, prof in enumerate(radprofs):
-		if raderrs is not None:
-			plt.errorbar(rbins*lmin, (rbins*lmin)**2*prof/(2*np.pi), yerr=(rbins*lmin)**2*raderrs[i]/(2*np.pi), marker='.', label=labels[i])
-		else:
-			plt.plot(rbins*lmin, (rbins*lmin)**2*prof/(2*np.pi), marker='.', label=labels[i])
+def plot_radavg_xspectrum2(rbins, radprofs=[], raderrs=None, labels=[], lmin=90., save=False, snspalette=None, pdf_or_png='png'):
+    
+    image_dim = 1024.
+    steradperpixel = ((np.pi/lmin)/image_dim)**2 
 
-	plt.legend()
-	plt.xscale('log')
-	plt.yscale('log')
-	plt.xlabel('$\\ell$', fontsize=14)
-	plt.ylabel('$\\ell(\\ell+1) C_{\\ell}/2\\pi$  $(nW m^{-2} sr^{-1})$', fontsize=14)
-	if save:
-		plt.savefig(figure_directory+'radavg_xspectrum.'+pdf_or_png, bbox_inches='tight')
-	plt.show()
+    plt.figure()
+    plt.title('Cross Spectrum', fontsize=16)
+    for i, prof in enumerate(radprofs):
+        if raderrs is not None:
+            plt.errorbar(rbins*lmin, np.sqrt((rbins*lmin)**2*prof/(2*np.pi)), yerr=(rbins*lmin)**2*raderrs[i]/(2*np.pi), marker='.', label=labels[i])
+        else:
+            plt.plot(rbins*lmin, np.sqrt((rbins*lmin)**2*prof/(2*np.pi)), marker='.', label=labels[i])
+
+    plt.legend()
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('$\\ell$', fontsize=14)
+    plt.ylabel('($\\ell(\\ell+1) C_{\\ell}/2\\pi)^(1/2)$  $(nW m^{-2} sr^{-1})$', fontsize=14)
+    if save:
+        plt.savefig(figure_directory+'radavg_xspectrum.'+pdf_or_png, bbox_inches='tight')
+    plt.show()
 
 
 def plot_2d_xspectrum(xspec, minpercentile=5, maxpercentile=95, save=False, pdf_or_png='png'):
