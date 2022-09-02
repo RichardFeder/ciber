fixed some bugs, generalizing pipeline to imagers/masking depths

ciber_mocks.py
@@ -19,6 +19,7 @@ import os
 from PIL import Image
 from image_eval import psf_poly_fit, image_model_eval
 import sys
+import time
 
 from numpy.fft import fftshift as fftshift
 from numpy.fft import ifftshift as ifftshift
@@ -516,16 +517,25 @@ class ciber_mock():
         return jansky_arr.to(u.nW*u.s/u.m**2)*const.c/(self.pix_sr*self.lam_effs[band])
 
     def make_srcmap_temp_bank(self, ifield, inst, cat,\
-                             flux_idx=-1, n_fine_bin=10, nwide=17,  beta=None, rc=None, norm=None, \
-                             tail_path='data/psf_model_dict_updated_081121_ciber.npz', verbose=False):
-
-        if beta is None:
-            beta, rc, norm = load_psf_params_dict(inst, ifield=ifield, tail_path=self.ciberdir+tail_path, verbose=verbose)
+                             flux_idx=-1, n_fine_bin=10, nwide=17, beta=None, rc=None, norm=None, \
+                             tail_path='data/psf_model_dict_updated_081121_ciber.npz', verbose=False, \
+                             tempbank_dirpath='data/subpixel_psf_temp_bank/', load_precomp_tempbank=False, compute_temp_bank=False):
 
 
-        if self.psf_temp_bank is None:
-            print('Generating PSF sub-pixel template bank for inst '+str(inst)+'..')
+        # todo load psf temps using inst and ifield
+
+        if load_precomp_tempbank:
+            print('Loading PSF template bank for inst '+str(inst)+', ifield '+str(ifield)+'..')
+            tempbank_fpath = tempbank_dirpath+'psf_temp_bank_inst'+str(inst)+'_ifield'+str(ifield)+'.npz'
+            self.psf_temp_bank = np.load(tempbank_fpath)['psf_temp_bank']
+        elif compute_temp_bank:
+            if beta is None:
+                beta, rc, norm = load_psf_params_dict(inst, ifield=ifield, tail_path=self.ciberdir+tail_path, verbose=verbose)
+            print('Generating PSF sub-pixel template bank for inst '+str(inst)+', ifield '+str(ifield)+'..')
             self.psf_temp_bank, dists = generate_psf_template_bank(beta, rc, norm, n_fine_bin=n_fine_bin, nwide=nwide)
+        else:
+            if self.psf_temp_bank is None:
+                print('no psf template bank in ciber_mock object..')
 
         dim_psf_post = nwide*2 + 1
         srcmap = np.zeros((self.nx*2, self.ny*2))
@@ -664,7 +674,9 @@ class ciber_mock():
             cat_full = np.hstack([cat_full, np.expand_dims(I_arr_full.value, axis=1)])
 
             if temp_bank:
-                srcmap_full = self.make_srcmap_temp_bank(ifield_list[c], inst, cat_full, flux_idx=-1, n_fine_bin=n_fine_bin, nwide=17)
+                srcmap_full = self.make_srcmap_temp_bank(ifield_list[c], inst, cat_full, flux_idx=-1, n_fine_bin=n_fine_bin, nwide=17, load_precomp_tempbank=True)
+                self.psf_temp_bank = None
+
             else:
                 srcmap_full = self.make_srcmap(ifield_list[c], cat_full, band=band, pcat_model_eval=pcat_model_eval, nwide=17)
 
@@ -787,9 +799,6 @@ class ciber_mock():
         number_ct_lims = np.array([17.5, 18.0, 18.5, 19.0])
         
         number_counts_array = np.zeros((nsims, len(ifield_list), len(number_ct_lims)))
-
-        if inst==2:
-            number_ct_lims -= 0.5
         
         make_fpaths([data_path, trilo_path])
         
@@ -811,9 +820,9 @@ class ciber_mock():
                 
                 cat_full = np.hstack([filt_x, np.expand_dims(star_Iarr_full_J.value, axis=1), np.expand_dims(star_Iarr_full_H.value, axis=1)])
                 
-                srcmap_stars_J = self.make_srcmap_temp_bank(ifield, 1, cat_full, flux_idx=4, n_fine_bin=10, nwide=17)
+                srcmap_stars_J = self.make_srcmap_temp_bank(ifield, 1, cat_full, flux_idx=4, n_fine_bin=10, nwide=17, load_precomp_tempbank=True)
                 self.psf_temp_bank = None
-                srcmap_stars_H = self.make_srcmap_temp_bank(ifield, 2, cat_full, flux_idx=5, n_fine_bin=10, nwide=17)
+                srcmap_stars_H = self.make_srcmap_temp_bank(ifield, 2, cat_full, flux_idx=5, n_fine_bin=10, nwide=17, load_precomp_tempbank=True)
 
                 full_maps_J.append(srcmap_stars_J)
                 full_maps_H.append(srcmap_stars_H)
@@ -863,7 +872,8 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
                               use_inst_mask = True, mag_lim_Vega=17.5, save_Mkk = True, save_mask = True, n_mkk_sims = 100,\
                               datestr = '062322', datestr_trilegal='062422', convert_AB_to_Vega = True, \
                              dm = 3., a1=160., b1=3.632, c1=8.0, \
-                             load_preff_joint_mask = False, include_ff_mask = False, interp_mask_fn_fpaths=None):
+                             load_preff_joint_mask = False, include_ff_mask = False, interp_mask_fn_fpaths=None, max_depth=8, \
+                             mag_depth_obs=17.0):
     
     Vega_to_AB = dict({1:0.91 , 2: 1.39}) # add 0.91 to get AB magnitude in J band
     mag_lim_AB = mag_lim_Vega + Vega_to_AB[inst]
@@ -873,15 +883,25 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
         param_combo = [a1, b1, c1]
 
     magkey_dict = dict({1:'j_m', 2:'h_m'})
+    inst_to_band = dict({1:'J', 2:'H'})
     magkey = magkey_dict[inst]
     
     imarray_shape = (len(ifield_list), cbps.dimx, cbps.dimy)
     
-    base_path = '/Volumes/Seagate Backup Plus Drive/Toolkit/Mirror/Richard/ciber_mocks/'
-    mask_fpath = base_path+datestr+'/TM'+str(inst)+'/masks/'
+    if dat_type=='mock':
+        base_path = '/Volumes/Seagate Backup Plus Drive/Toolkit/Mirror/Richard/ciber_mocks/'
+        mask_fpath = base_path+datestr+'/TM'+str(inst)+'/masks/'
+        mkk_fpath = base_path+datestr+'/TM'+str(inst)+'/mkk/'
+        mkk_ff_fpath = mkk_fpath+'ff_joint_masks/'
+
+    elif dat_type=='real':
+        base_path = 'data/fluctuation_data/'
+        mask_fpath = base_path+'TM'+str(inst)+'/masks/'
+        mkk_fpath = base_path+'TM'+str(inst)+'/mkk/'
+        mkk_ff_fpath = mkk_fpath+'ff_joint_masks/'
+
     ff_joint_mask_fpath = mask_fpath+'ff_joint_masks/'
-    mkk_fpath = base_path+datestr+'/TM'+str(inst)+'/mkk/'
-    mkk_ff_fpath = mkk_fpath+'ff_joint_masks/'
+
 
     ciber_field_dict = dict({4:'elat10', 5:'elat30',6:'BootesB', 7:'BootesA', 8:'SWIRE'})
 
@@ -927,6 +947,9 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
             if use_inst_mask:
                 cbps.load_mask(ifield, inst, masktype='maskInst_clean')
                 joint_mask *= cbps.maskInst_clean
+
+                # cbps.load_mask(ifield, inst, masktype='maskInst')
+                # joint_mask *= cbps.maskInst
                 
             if generate_starmask: 
 
@@ -935,18 +958,28 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
                     mock_trilegal = fits.open(mock_trilegal_path)
                     mock_trilegal_cat = mock_trilegal['tracer_cat_'+str(ifield)].data
                 elif dat_type=='real':
-                    trilegal_fpath = ''
-                    mock_trilegal_cat = []
+                    # these are already in Vega magnitudes
+                    twomass_lt_16 = pd.read_csv('data/cats/masking_cats/'+field_name+'/twomass_lt_16_maskcat_'+field_name+'_082922.csv')
+                    twomass_x = twomass_lt_16['x'+str(inst)]
+                    twomass_y = twomass_lt_16['y'+str(inst)]
+                    twomass_mag = twomass_lt_16[magkey_dict[inst]]
+                    twomass_cat = np.array([twomass_x, twomass_y, twomass_mag]).transpose()
 
-                if convert_AB_to_Vega:
+                if convert_AB_to_Vega and dat_type=='mock':
                     print('Converting AB magnitudes to Vega, subtracting by ', Vega_to_AB[inst])
                     mock_trilegal_cat[magkey_dict[inst]] -= Vega_to_AB[inst]
-
-                print('mock trilegal cat has shape', mock_trilegal_cat.shape)
                 
-                star_cat = {magkey:mock_trilegal_cat[magkey].byteswap().newbyteorder(), 'x'+str(inst):mock_trilegal_cat['x'].byteswap().newbyteorder(), 'y'+str(inst):mock_trilegal_cat['y'].byteswap().newbyteorder()}
-                star_cat_df = pd.DataFrame(star_cat)
-                star_cat_df.columns = [magkey, 'x'+str(inst), 'y'+str(inst)]
+                starcatcols = [magkey, 'x'+str(inst), 'y'+str(inst)]
+                if dat_type=='mock':
+                    print('mock trilegal cat has shape', mock_trilegal_cat.shape)
+
+                    star_cat_df = pd.DataFrame({magkey:mock_trilegal_cat[magkey].byteswap().newbyteorder(), 'x'+str(inst):mock_trilegal_cat['x'].byteswap().newbyteorder(), 'y'+str(inst):mock_trilegal_cat['y'].byteswap().newbyteorder()}, \
+                        columns=starcatcols)
+
+                elif dat_type=='real':
+                    star_cat_df = pd.DataFrame({magkey:twomass_cat[:,2], 'x'+str(inst):twomass_cat[:,0],'y'+str(inst):twomass_cat[:,1]}, columns=starcatcols)
+                
+                # star_cat_df.columns = [magkey, 'x'+str(inst), 'y'+str(inst)]
                 
                 if interp_mask_fn_fpaths is not None:
                     # simulated sources changed to Vega magnitudes with convert_AB_to_Vega, masking function magnitudes in Vega units
@@ -962,18 +995,32 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
                 joint_mask *= starmask
 
             if generate_galmask:
-                midxdict = dict({'x':0, 'y':1, 'redshift':2, 'm_app':3, 'M_abs':4, 'Mh':5, 'Rvir':6})
-                mock_gal = fits.open(base_path+datestr+'/TM'+str(inst)+'/cib_with_tracer_5field_set'+str(sim_idx)+'_'+datestr+'_TM'+str(inst)+'.fits')
-                mock_gal_cat = mock_gal['tracer_cat_'+str(ifield)].data
 
-                if convert_AB_to_Vega:
+                if dat_type=='mock':
+                    midxdict = dict({'x':0, 'y':1, 'redshift':2, 'm_app':3, 'M_abs':4, 'Mh':5, 'Rvir':6})
+                    mock_gal = fits.open(base_path+datestr+'/TM'+str(inst)+'/cib_with_tracer_5field_set'+str(sim_idx)+'_'+datestr+'_TM'+str(inst)+'.fits')
+                    mock_gal_cat = mock_gal['tracer_cat_'+str(ifield)].data
+                else:
+                    unWISE_PS_cat_fpath = 'data/cats/masking_cats/'+field_name+'/unWISE_PanSTARRS_dt_mask_cat_'+field_name+'_maxdepth='+str(max_depth)+'_'+str(inst_to_band[inst])+'lim='+str(max(mag_lim_Vega, mag_depth_obs))+'_inst'+str(inst)+'_082922.csv'
+                    print('Loading fainter catalog objects from ', unWISE_PS_cat_fpath)
+                    mask_cat_unWISE_PS = pd.read_csv(unWISE_PS_cat_fpath)
+                    faint_cat_x = mask_cat_unWISE_PS['x'+str(inst)]
+                    faint_cat_y = mask_cat_unWISE_PS['y'+str(inst)]
+                    faint_cat_mag = mask_cat_unWISE_PS[inst_to_band[inst]+'_predict']
+
+                if convert_AB_to_Vega and dat_type=='mock':
                     print('converting galaxy magnitudes to Vega mag with ', Vega_to_AB[inst])
                     mock_gal_cat['m_app'] -= Vega_to_AB[inst]
 
-                gal_cat = {'m_app':mock_gal_cat['m_app'].byteswap().newbyteorder(), 'x'+str(inst):mock_gal_cat['x'].byteswap().newbyteorder(), 'y'+str(inst):mock_gal_cat['y'].byteswap().newbyteorder()}
-                gal_cat_df = pd.DataFrame(gal_cat, columns = ['m_app', 'x'+str(inst), 'y'+str(inst)]) # check magnitude system of Helgason model
-                
-                if interp_mask_fn_fpaths is not None:                    
+                galcatcols = ['m_app', 'x'+str(inst), 'y'+str(inst)]
+                if dat_type=='mock':
+                    gal_cat = {'m_app':mock_gal_cat['m_app'].byteswap().newbyteorder(), 'x'+str(inst):mock_gal_cat['x'].byteswap().newbyteorder(), 'y'+str(inst):mock_gal_cat['y'].byteswap().newbyteorder()}
+                    gal_cat_df = pd.DataFrame(gal_cat, columns = galcatcols) # check magnitude system of Helgason model
+                elif dat_type=='real':
+                    gal_cat_df = pd.DataFrame({'m_app':faint_cat_mag, 'x'+str(inst):faint_cat_x, 'y'+str(inst):faint_cat_y}, columns = galcatcols)
+
+                if interp_mask_fn_fpaths is not None:
+
                     galmask, radii_gals = mask_from_cat(cat_df = gal_cat_df, mag_lim_min=0, inst=inst,\
                                             mag_lim=mag_lim_Vega, interp_maskfn=interp_maskfn, magstr='m_app', Vega_to_AB=0., dimx=cbps.dimx, dimy=cbps.dimy, plot=False, \
                                                        interp_max_mag = max_mag)
@@ -984,6 +1031,8 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
                 if len(radii_gals) > 0:
                     print('len radii gals is ', len(radii_gals))
                     joint_mask *= galmask
+                else:
+                    print('radii gals is ', radii_gals, 'and galmask is ', galmask)
 
             print(float(np.sum(joint_mask))/float(1024**2))
 
@@ -999,7 +1048,12 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
                                       generate_starmask=generate_starmask, use_inst_mask=use_inst_mask, dat_type=dat_type, mag_lim_AB=mag_lim_AB, \
                                       a1=a1, b1=b1, c1=c1, dm=dm, alpha_m=alpha_m, beta_m=beta_m)
 
-                hdul.writeto(mask_fpath+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(sim_idx)+'_'+masktail+'.fits', overwrite=True)
+                if dat_type=='mock':
+                    mask_save_fpath = mask_fpath+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(sim_idx)+'_'+masktail+'.fits'
+                elif dat_type=='real':
+                    mask_save_fpath = mask_fpath+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_observed_'+masktail+'.fits'
+                print('Saving mask to ', mask_save_fpath)
+                hdul.writeto(mask_save_fpath, overwrite=True)
 
             if save_Mkk:
 
@@ -1011,8 +1065,13 @@ def generate_full_mask_and_mll(cbps, ifield_list, sim_idxs, masktail, inst = 1, 
                 hdul = write_Mkk_fits(Mkk, inv_Mkk, ifield, inst, sim_idx=sim_idx, generate_starmask=generate_starmask, generate_galmask=generate_galmask, \
                                      use_inst_mask=use_inst_mask, dat_type=dat_type, mag_lim_AB=mag_lim_Vega)
 
+                if dat_type=='mock':
+                    mkk_save_fpath = mkk_fpath+'mkk_estimate_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(sim_idx)+'_'+masktail+'.fits'
+                elif dat_type=='real':
+                    mkk_save_fpath = mkk_fpath+'mkk_estimate_ifield'+str(ifield)+'_inst'+str(inst)+'_observed_'+masktail+'.fits'
+                print('Saving Mkk matrix and inverse to ', mkk_save_fpath)
 
-                hdul.writeto(mkk_fpath+'mkk_estimate_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(sim_idx)+'_'+masktail+'.fits', overwrite=True)
+                hdul.writeto(mkk_save_fpath, overwrite=True)
 
 
     

ciber_powerspec_pipeline.py
@@ -215,7 +215,6 @@ def iterative_gradient_ff_solve(orig_images, niter=3, masks=None, weights_ff=Non
 	return images, ff_estimates, final_planes, stack_masks, all_coeffs
 
 
-
 def compute_residual_source_shot_noise(mag_lim_list, inst, ifield_list, datestr, mode='cib', cmock=None, cbps=None, convert_Vega_to_AB=True, \
                                     simidx0=0, nsims=100, ifield_plot=4, save=True, \
                                       ciber_mock_dirpath='/Volumes/Seagate Backup Plus Drive/Toolkit/Mirror/Richard/ciber_mocks/'):
@@ -277,7 +276,8 @@ def compute_residual_source_shot_noise(mag_lim_list, inst, ifield_list, datestr,
                 if convert_Vega_to_AB:
                     if m==0:
                         print('converting mag limit threshold from Vega to AB to match mock catalog..')
-                    print('AB mag lim for '+bandstr+' is ', mag_lim+cmock.Vega_to_AB[inst])
+                    if setidx==0:
+                    	print('AB mag lim for '+bandstr+' is ', mag_lim+cmock.Vega_to_AB[inst])
                     mag_limit_match = mag_lim + cmock.Vega_to_AB[inst]
                 else:
                     if m==0:
@@ -286,8 +286,12 @@ def compute_residual_source_shot_noise(mag_lim_list, inst, ifield_list, datestr,
                     
                 bright_cut = np.where(full_tracer_cat[2,:] < mag_limit_match)[0]
                 cut_cat = full_tracer_cat[:, bright_cut].transpose()
-                bright_src_map = cmock.make_srcmap_temp_bank(ifield, inst, cut_cat, flux_idx=-1)
-
+                if m==0:
+                	load_precomp_tempbank = True 
+                else:
+                	load_precomp_tempbank = False
+                bright_src_map = cmock.make_srcmap_temp_bank(ifield, inst, cut_cat, flux_idx=-1, load_precomp_tempbank=load_precomp_tempbank)
+                
                 diff_full_bright = full_src_map - bright_src_map
                 diff_full_bright -= np.mean(diff_full_bright)
                 
@@ -320,7 +324,8 @@ def compute_residual_source_shot_noise(mag_lim_list, inst, ifield_list, datestr,
                 prim = fits.PrimaryHDU(header=hdr)
                 hdul = fits.HDUList([prim, hdu_cl])
                 hdul.writeto(cl_save_fpath, overwrite=True)
-
+            
+            cmock.psf_temp_bank = None
                 
             if ifield==ifield_plot:
                 cl_table_test = fits.open(cl_save_fpath)['cls_'+mode].data
@@ -337,8 +342,6 @@ def compute_residual_source_shot_noise(mag_lim_list, inst, ifield_list, datestr,
         return isl_sb_rms
     elif mode=='cib':
         return None
-        
-
 
 	
 class CIBER_PS_pipeline():
@@ -1332,7 +1335,7 @@ class CIBER_PS_pipeline():
 
 
 
-	def calculate_transfer_function(self, inst, nsims, load_ptsrc_cib=False, niter_grad_ff=1, dgl_scale_fac=5., indiv_ifield=6, apply_FF=False, FF_image=None, FF_fpath=None, plot=False):
+	def calculate_transfer_function(self, nsims, load_ptsrc_cib=False, niter_grad_ff=1, dgl_scale_fac=5., indiv_ifield=6, apply_FF=False, FF_image=None, FF_fpath=None, plot=False):
 
 		''' 
 		Estimate the transfer function of gradient filtering by passing through a number of sky realizations and measuring the ratio of input/output power spectra.
@@ -1374,12 +1377,12 @@ class CIBER_PS_pipeline():
 			for i in range(nsims):
 
 				diff_realization = self.generate_custom_sky_clustering(dgl_scale_fac=dgl_scale_fac, gen_ifield=indiv_ifield)
-				if not load_ptsrc_cib:        
-					_, _, shotcomp = generate_diffuse_realization(self.dimx, self.dimy, power_law_idx=0.0, scale_fac=3e8)
-				else:
-					print('need to load CIB here')
-
+				# if not load_ptsrc_cib:        
+				_, _, shotcomp = generate_diffuse_realization(self.dimx, self.dimy, power_law_idx=0.0, scale_fac=3e8)
 				diff_realization += shotcomp 
+
+				# else:
+					# print('need to load CIB here')
 
 				if plot:
 					plot_map(diff_realization, title='diff realization '+str(i))
@@ -1563,10 +1566,10 @@ class CIBER_PS_pipeline():
 
 		return lbins, cl_proc, cl_proc_err, masked_image
 
-	def estimate_b_ell_from_maps(self, inst, ifield_list, ciber_maps, simidx=0, ell_norm=5000, plot=False, save=False, \
+	def estimate_b_ell_from_maps(self, inst, ifield_list, ciber_maps, simidx=0, ell_norm=5000, plot=True, save=False, \
 										niter = 5, ff_stack_min=2, data_type='mock', datestr_mock='062322', datestr_trilegal=None, \
 										ciber_mock_fpath='/Volumes/Seagate Backup Plus Drive/Toolkit/Mirror/Richard/ciber_mocks/', \
-										mask_base_path=None, mask_tail='maglim_17_Vega', bright_mask_tail='maglim_11_Vega'):
+										mask_base_path=None, full_mask_tail='maglim_17_Vega_test', bright_mask_tail='maglim_11_Vega_test'):
 
 		
 		''' 
@@ -1602,6 +1605,8 @@ class CIBER_PS_pipeline():
 		
 		'''
 		
+		print('full mask tail:', full_mask_tail)
+		print('bright mask tail:', bright_mask_tail)
 		ps_set_shape = (len(ifield_list), self.n_ps_bin)
 		imarray_shape = (len(ifield_list),self.dimx, self.dimy)
 		diff_norm_array, cls_masked_tot, cls_masked_tot_bright = [np.zeros(ps_set_shape) for x in range(3)]
@@ -1621,7 +1626,7 @@ class CIBER_PS_pipeline():
 
 
 			if data_type=='mock':
-				joint_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(simidx)+'_'+mask_tail+'.fits')['joint_mask_'+str(ifield)].data
+				joint_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(simidx)+'_'+full_mask_tail+'.fits')['joint_mask_'+str(ifield)].data
 				bright_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(simidx)+'_'+bright_mask_tail+'.fits')['joint_mask_'+str(ifield)].data
 				tot_masks[fieldidx] = joint_mask
 				bright_masks[fieldidx] = bright_mask
@@ -1633,11 +1638,20 @@ class CIBER_PS_pipeline():
 			else:
 				self.load_data_products(ifield, inst, verbose=False)
 
-				joint_mask = fits.open(joint_mask_fpaths[fieldidx])[0].data
-				bright_mask = fits.open(bright_mask_fpaths[fieldidx])[0].data
+				# joint_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_observed_'+full_mask_tail+'.fits')['joint_mask_'+str(ifield)].data
+				# bright_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_observed_'+bright_mask_tail+'.fits')['joint_mask_'+str(ifield)].data
+				joint_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_observed_'+full_mask_tail+'.fits')[1].data
+				bright_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_observed_'+bright_mask_tail+'.fits')[1].data
+
+				# joint_maskos[fieldidx] = fits.open(mask_fpath)[1].data
+				# joint_mask = fits.open(joint_mask_fpaths[fieldidx])[0].data
+				# bright_mask = fits.open(bright_mask_fpaths[fieldidx])[0].data
 
 				tot_masks[fieldidx] = joint_mask
-				bright_masks[fieldidx] = bright_mask*self.maskInst
+				bright_masks[fieldidx] = bright_mask
+
+				plot_map(tot_masks[fieldidx]*ciber_maps[fieldidx], title='tot mask x ciber maps')
+				plot_map(bright_masks[fieldidx]*ciber_maps[fieldidx], title='bright mask x ciber maps')
 
 
 				
@@ -1887,159 +1901,6 @@ class CIBER_PS_pipeline():
 		return joint_masks, observed_ims, total_signals, rnmaps, shot_sigma_sb_maps, noise_models, ff_truth, diff_realizations, zl_perfield
 	   
 
-
-			
-
-# def estimate_b_ell_from_maps(cbps, inst, ifield_list, ciber_maps, simidx=0, pixsize=7., J_bright_mag=11., J_tot_mag=17.5, ell_norm=10000, plot=False, save=False, \
-#                                     niter = 5, ff_stack_min=2, data_type='mock', datestr_mock='062322', datestr_trilegal=None, \
-#                                     ciber_mock_fpath='/Volumes/Seagate Backup Plus Drive/Toolkit/Mirror/Richard/ciber_mocks/', \
-#                                     mask_base_path=None, mask_tail='maglim_17_Vega', bright_mask_tail='maglim_11_Vega'):
-
-	
-#     ''' 
-#     This function estimates the beam correction function from the maps. This is done by computing the power spectrum 
-#     of the point source dominated maps and then subtracting the power spectrum of masked maps to remove noise bias. The final power
-#     spectrum is then normalized to unity at a pivot ell value. 
-	
-#     Note that there is some numerical weirdness with doing this on individual realizations when normalizing the power spectrum
-#     at lower ell. This is fixed by either assuming the beam transfer function is >0.999 below some multipole, or by averaging over several realizations.
-	
-#     Parameters
-#     ----------
-	
-#     inst : 'int'. Instrument index, 1==J, 2==H
-#     ifield_list : 'list' of 'ints'. 
-#     pixsize : 'float'.
-#         Default is 7.
-#     J_bright_mag : 'float'.
-#     J_tot_mag : 'float'.
-#     nsim : 'int'. Number of realizations.
-#         Default is 1.
-#     ell_norm : 'float'. Multipole used to normalize power spectra for B_ell estimate. 
-#         Default is 10000. 
-#     niter : 'int'. Number of iterations on FF/grad estimation
-#     ff_stack_min : 'int'. Minimum number of off-field measurements for each pixel.
-#     data_type : 'string'. Distinguishes between observed and mock data.
-#         Default is 'mock'.
-	
-#     Returns
-#     -------
-#     lb : 'np.array' of 'floats'. Power spectrum multipole bin centers.
-#     diff_norm : normalized profiles
-	
-#     '''
-	
-#     ps_set_shape = (len(ifield_list), cbps.n_ps_bin)
-#     imarray_shape = (len(ifield_list),cbps.dimx, cbps.dimy)
-#     diff_norm_array, cls_masked_tot, cls_masked_tot_bright = [np.zeros(ps_set_shape) for x in range(3)]
-	
-#     if datestr_trilegal is None:
-#         datestr_trilegal = datestr_mock
-		
-#     if mask_base_path is None:
-#         mask_base_path = base_path+'TM'+str(inst)+'/masks/'
-		
-#     print('simidx is ', simidx)
-
-#     tot_masks, bright_masks, tot_obs = [np.zeros(imarray_shape) for x in range(3)]
-
-#     for fieldidx, ifield in enumerate(ifield_list):
-#         print('ifield = ', ifield)
-
-
-#         if data_type=='mock':
-#             joint_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(simidx)+'_'+mask_tail+'.fits')['joint_mask_'+str(ifield)].data
-#             bright_mask = fits.open(mask_base_path+'joint_mask_ifield'+str(ifield)+'_inst'+str(inst)+'_simidx'+str(simidx)+'_'+bright_mask_tail+'.fits')['joint_mask_'+str(ifield)].data
-#             tot_masks[fieldidx] = joint_mask
-#             bright_masks[fieldidx] = bright_mask
-			
-#             if fieldidx==0:
-#                 plot_map(tot_masks[fieldidx]*ciber_maps[fieldidx], title='tot mask x ciber maps')
-#                 plot_map(bright_masks[fieldidx]*ciber_maps[fieldidx], title='bright mask x ciber maps')
-				
-#         else:
-#             cbps.load_data_products(ifield, inst, verbose=False)
-
-#             joint_mask = fits.open(joint_mask_fpaths[fieldidx])[0].data
-#             bright_mask = fits.open(bright_mask_fpaths[fieldidx])[0].data
-
-#             tot_masks[fieldidx] = joint_mask
-#             bright_masks[fieldidx] = bright_mask*cbps.maskInst
-
-
-			
-#     print('Computing gradients/flat fields using bright masks')
-
-#     bright_images, ff_estimates_bright, final_planes,\
-#             stack_masks_bright, all_coeffs = iterative_gradient_ff_solve(ciber_maps, \
-#                                                                   niter=niter, masks=bright_masks, \
-#                                                                  ff_stack_min=ff_stack_min)
-
-#     print('computing for total mask')
-#     tot_images, ff_estimates_tot, final_planes,\
-#             stack_masks_tot, all_coeffs = iterative_gradient_ff_solve(ciber_maps, \
-#                                                                   niter=niter, masks=tot_masks, \
-#                                                                  ff_stack_min=ff_stack_min)
-
-
-#     print('now computing power spectra of maps..')
-#     for fieldidx, ifield in enumerate(ifield_list):
-
-#         print('computing point source dominated power spectrum..')
-#         # compute point source dominated power spectrum
-#         fullmaskbright = stack_masks_bright[fieldidx]*bright_masks[fieldidx]
-#         masked_bright_meansub = ciber_maps[fieldidx]*fullmaskbright
-
-#         masked_bright_meansub[fullmaskbright != 0] -= np.mean(masked_bright_meansub[fullmaskbright != 0])
-#         lb, cl_masked_tot_bright, clerr_masked = get_power_spec(masked_bright_meansub, lbinedges=cbps.Mkk_obj.binl, lbins=cbps.Mkk_obj.midbin_ell)
-
-#         cls_masked_tot_bright[fieldidx] = cl_masked_tot_bright
-		
-#         print('computing masked noise power spectrum..')
-#         # compute masked noise power spectrum
-#         masked_tot_meansub = ciber_maps[fieldidx]*tot_masks[fieldidx]*stack_masks_tot[fieldidx]
-#         masked_tot_meansub[tot_masks[fieldidx]*stack_masks_tot[fieldidx] != 0] -= np.mean(masked_tot_meansub[tot_masks[fieldidx]*stack_masks_tot[fieldidx] != 0])
-#         lb, cl_masked_tot, clerr_masked = get_power_spec(masked_tot_meansub, lbinedges=cbps.Mkk_obj.binl, lbins=cbps.Mkk_obj.midbin_ell)
-
-#         cls_masked_tot[fieldidx] = cl_masked_tot
-
-#         # compute power spectrum difference
-#         bright_faint_diff_cl = cl_masked_tot_bright - cl_masked_tot
-
-#         if plot:
-#             plt.figure(figsize=(8,6))
-#             prefac = lb**2/(2*np.pi)
-#             plt.plot(lb, prefac*cl_masked_tot_bright, label='J < 10 masked')
-#             plt.plot(lb, prefac*cl_masked_tot, label='masked J < 18')
-#             plt.plot(lb, prefac*bright_faint_diff_cl, label='difference')
-#             plt.xscale('log')
-#             plt.legend(fontsize=14)
-#             plt.yscale('log')
-#             plt.tick_params(labelsize=14)
-#             plt.show()
-
-#         print('Normalizing power spectra..')
-
-#         # normalize power spectrum by maximum value for ell > ell_norm
-#         diff_norm = bright_faint_diff_cl / np.max(bright_faint_diff_cl[lb > ell_norm])
-#         # anything for ell < ell_norm that is greater than one set to one
-#         diff_norm[diff_norm > 1] = 1.
-
-#         diff_norm_array[fieldidx, :] = diff_norm 
-
-#         if plot:
-#             plt.figure()
-#             plt.plot(lb, diff_norm, label='flight map differenced')
-# #             plt.plot(lb, B_ells[fieldidx]**2, label='stacking B ell')
-#             plt.ylabel('$B_{\\ell}^2$', fontsize=18)
-#             plt.xlabel('Multipole $\\ell$', fontsize=18)
-#             plt.yscale('log')
-#             plt.xscale('log')
-#             plt.legend()
-#             plt.show()
-
-			
-#     return lb, diff_norm_array, cls_masked_tot, cls_masked_tot_bright
 
 
 

masking_utils.py
@@ -115,25 +115,26 @@ def mask_from_cat(xs=None, ys=None, mags=None, cat_df=None, dimx=1024, dimy=1024
     if interp_maskfn is not None:
         # magnitudes need to be in Vega system as this is how interp_maskfn is defined!! 
         print("Using interpolated function to get masking radii..")
-        if cat_df is not None:
+        if cat_df is not None and len(cat_df) > 0:
             mags = cat_df[magstr]
 
-        print('max mag is ', np.max(mags), np.nanmax(mags))
-        print('interp max mag is ', interp_max_mag)
+            print('max mag is ', np.max(mags), np.nanmax(mags))
+            print('interp max mag is ', interp_max_mag)
 
-        if interp_max_mag is not None:
-            mags[mags > interp_max_mag] = interp_max_mag
-        if interp_min_mag is not None:
-            mags[mags < interp_min_mag] = interp_min_mag
+            if interp_max_mag is not None:
+                mags[mags > interp_max_mag] = interp_max_mag
+            if interp_min_mag is not None:
+                mags[mags < interp_min_mag] = interp_min_mag
 
-
-        radii = interp_maskfn(np.array(mags))
-        if plot:
-            plt.figure()
-            plt.scatter(mags, radii, s=3, color='k')
-            plt.xlabel('Vega mags')
-            plt.ylabel('radii [arcsec]')
-            plt.show()
+            radii = interp_maskfn(np.array(mags))
+            if plot:
+                plt.figure()
+                plt.scatter(mags, radii, s=3, color='k')
+                plt.xlabel('Vega mags')
+                plt.ylabel('radii [arcsec]')
+                plt.show()
+        else:
+            return None, []
 
     if compute_radii and radii is None:
         print('Computing radii based on magnitudes..')