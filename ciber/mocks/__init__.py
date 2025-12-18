80344b610af4008a49d4f517b1f4550227f57814
Richard Feder <richardfeder@Richards-MacBook-Pro-6.local>
2025-12-17 18:55:59 -0800

    Rename mock files back to original names
    - j_band_proc.py → proc_jmocks.py
    - mock_gal_gross.py → mock_gal_cross.py
    
    Updated imports in mocks/__init__.py accordingly.

--- ciber/mocks/__init__.py
+++ ciber/mocks/__init__.py
@@ -9,8 +9,8 @@ from .cib_mocks import *
 from .galaxy_catalogs import *
 from .grigory_mocks import *
 from .lognormal import *
-from .mock_gal_gross import *
-from .j_band_proc import *
+from .mock_gal_cross import *
+from .proc_jmocks import *
 
 __all__ = ['cib_mocks', 'galaxy_catalogs', 'grigory_mocks', 'lognormal', 
-           'mock_gal_gross', 'j_band_proc']
+           'mock_gal_cross', 'proc_jmocks']
